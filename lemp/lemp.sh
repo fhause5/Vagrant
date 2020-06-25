b18233ac63b5e358b214deb7e6f044ef41566632
Igor <45092677+fhause5@users.noreply.github.com>
2020-06-25 10:39:57 +0300

    Update lemp.sh

--- lemp/lemp.sh
+++ lemp/lemp.sh
@@ -28,7 +28,9 @@ setfacl -m user:vagrant:rw /var/run/docker.sock
 
 apt install -y mysql-server
 apt install -y php php-fpm php-mysql php-xml php-mbstring
-#apt install -y apt install php7.2-fpm php7.2-common php7.2-sqlite3 php7.2-mysql php7.2-gmp php7.2-curl php7.2-intl php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-ldap php7.2-gd php7.2-bcmath php7.2-xml php7.2-cli php7.2-zip
+apt install -y apt install php7.2-fpm php7.2-common php7.2-sqlite3 php7.2-mysql php7.2-gmp php7.2-curl php7.2-intl php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-ldap php7.2-gd php7.2-bcmath php7.2-xml php7.2-cli php7.2-zip
+
+echo "<?php phpinfo ();?>" | sudo tee /var/www/html/info.php
 
 # Composer
 
